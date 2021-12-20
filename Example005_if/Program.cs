f40eb78b52c073dd61e8acf50b111c89f1b997a6
Elena <zara-79@bk.ru>
2021-12-20 08:31:54 +0300

    Ex005

---
+++ Example005_if/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "лена")
+{
+    System.Console.WriteLine("Ура, это же ЛЕНА!");
+}
+else 
+{
+    System.Console.Write("Привет, ");
+    System.Console.WriteLine(username);
+}
